5f3fdb7fd1d452edd13c9825a3db0af8130cf092
Shubhamf0073 <shubhamfufal.8@outlook.com>
2025-05-06 02:42:10 +0530

    Added files for preprocessing

--- DataPrep/DataMerging.py
+++ DataPrep/DataMerging.py
@@ -1,2 +1,26 @@
 import pandas as pd
 
+filePaths = [
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d0.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d1.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d2.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d3.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d4.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d5.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d6_from_parquet.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d7.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/d8_from_parquet.csv",
+    "/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/convoStarter - Sheet1.csv"
+]
+
+dfs = [pd.read_csv(filePath) for filePath in filePaths]
+
+mergedDf = pd.concat(dfs, ignore_index = True)
+
+print(f"Merged df shape: {mergedDf.shape}")
+print(f"Merged df columns: {mergedDf.columns}")
+print(f"Merged df head: {mergedDf.head()}")
+
+mergedDf.to_csv("/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/final.csv", index = False)
+
+print("Merged data saved as final.csv")
---
+++ DataPrep/preProcessing.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("/Users/shubhamfufal/Library/Mobile Documents/com~apple~CloudDocs/2ndSemProject/Bot/Data/final.csv")
+
+print(f"Columns: {df.columns}")
